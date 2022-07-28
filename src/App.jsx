4fcd670b6482094a40fd634cddb67b70f2820dc9
Hatem Soliman <hatemthedev@gmail.com>
2022-07-28 17:12:37 +0200

    Adding custom styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,27 @@ function App() {
   const { search, animals } = useAnimalSearch();
 
   return (
-    <main>
-      <h1>Animal Farm</h1>
+    <div className="container">
+      <div className="row">
+        <main>
+          <h1>Animal Farm</h1>
 
-      <input
-        type="text"
-        placeholder="Search"
-        onChange={(e) => search(e.target.value)}
-      />
+          <input
+            type="text"
+            placeholder="Search"
+            onChange={(e) => search(e.target.value)}
+          />
 
-      <ul>
-        {animals.map((animal) => (
-          <Animal key={animal.id} {...animal} />
-        ))}
+          <ol>
+            {animals.map((animal) => (
+              <Animal key={animal.id} {...animal} />
+            ))}
 
-        {animals.length === 0 && 'No animals found'}
-      </ul>
-    </main>
+            {animals.length === 0 && 'No animals found'}
+          </ol>
+        </main>
+      </div>
+    </div>
   );
 }
 
