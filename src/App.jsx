ffd8199ea9ef284825a00c3d26ab8b57b5bec57d
Hatem Soliman <hatemthedev@gmail.com>
2022-07-28 22:10:46 +0200

    Added bg photo credit

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,31 @@ function App() {
   const { search, animals } = useAnimalSearch();
 
   return (
-    <div className="container">
-      <div className="row">
-        <main>
-          <h1>Animal Farm</h1>
+    <>
+      <div className="container">
+        <div className="row">
+          <main>
+            <h1>Animal Farm</h1>
 
-          <input
-            type="text"
-            placeholder="Search"
-            onChange={(e) => search(e.target.value)}
-          />
+            <input
+              type="text"
+              placeholder="Search"
+              onChange={(e) => search(e.target.value)}
+            />
 
-          <ol>
-            {animals.map((animal) => (
-              <Animal key={animal.id} {...animal} />
-            ))}
+            <ol>
+              {animals.map((animal) => (
+                <Animal key={animal.id} {...animal} />
+              ))}
 
-            {animals.length === 0 && 'No animals found'}
-          </ol>
-        </main>
+              {animals.length === 0 && 'No animals found'}
+            </ol>
+          </main>
+        </div>
       </div>
-    </div>
+      <span>Photo by <a href="https://unsplash.com/@andyjh07?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Andy Holmes</a> on <a href="https://unsplash.com/s/photos/animals?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+      </span>
+    </>
   );
 }
 
